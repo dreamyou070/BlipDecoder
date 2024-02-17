01474098875461c3768c7b6ccc334f691db52a8e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 23:33:25 +0900

    a

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from models.blip import blip_decoder
 import torch
 import torch.nn as nn
-from transformers import BertConfig, BertLMHeadModel, BertTokenizer
+from models.med import BertConfig, BertLMHeadModel
+from transformers import BertTokenizer
 from transformers import VisionTransformer
 
 def init_tokenizer():
@@ -42,10 +43,7 @@ class BLIP_Decoder(nn.Module):
 
         image_embeds = self.visual_encoder(image)
         image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(image.device)
-
-        text = self.tokenizer(caption, padding='longest', truncation=True, max_length=40, return_tensors="pt").to(
-            image.device)
-
+        text = self.tokenizer(caption, padding='longest', truncation=True, max_length=40, return_tensors="pt").to(image.device)
         text.input_ids[:, 0] = self.tokenizer.bos_token_id
 
         decoder_targets = text.input_ids.masked_fill(text.input_ids == self.tokenizer.pad_token_id, -100)
@@ -53,58 +51,15 @@ class BLIP_Decoder(nn.Module):
 
         decoder_output = self.text_decoder(text.input_ids,
                                            attention_mask=text.attention_mask,
+
                                            encoder_hidden_states=image_embeds,
                                            encoder_attention_mask=image_atts,
-                                           labels=decoder_targets,
-                                           return_dict=True,
-                                           )
-        loss_lm = decoder_output.loss
 
+                                           labels=decoder_targets,
+                                           return_dict=True,)
+        loss_lm = decoder_output.loss
         return loss_lm
 
-    def generate(self, image, sample=False, num_beams=3, max_length=30, min_length=10, top_p=0.9,
-                 repetition_penalty=1.0):
-        image_embeds = self.visual_encoder(image)
-
-        if not sample:
-            image_embeds = image_embeds.repeat_interleave(num_beams, dim=0)
-
-        image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(image.device)
-        model_kwargs = {"encoder_hidden_states": image_embeds, "encoder_attention_mask": image_atts}
-
-        prompt = [self.prompt] * image.size(0)
-        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids.to(image.device)
-        input_ids[:, 0] = self.tokenizer.bos_token_id
-        input_ids = input_ids[:, :-1]
-
-        if sample:
-            # nucleus sampling
-            outputs = self.text_decoder.generate(input_ids=input_ids,
-                                                 max_length=max_length,
-                                                 min_length=min_length,
-                                                 do_sample=True,
-                                                 top_p=top_p,
-                                                 num_return_sequences=1,
-                                                 eos_token_id=self.tokenizer.sep_token_id,
-                                                 pad_token_id=self.tokenizer.pad_token_id,
-                                                 repetition_penalty=1.1,
-                                                 **model_kwargs)
-        else:
-            # beam search
-            outputs = self.text_decoder.generate(input_ids=input_ids,
-                                                 max_length=max_length,
-                                                 min_length=min_length,
-                                                 num_beams=num_beams,
-                                                 eos_token_id=self.tokenizer.sep_token_id,
-                                                 pad_token_id=self.tokenizer.pad_token_id,
-                                                 repetition_penalty=repetition_penalty,
-                                                 **model_kwargs)
-
-        captions = []
-        for output in outputs:
-            caption = self.tokenizer.decode(output, skip_special_tokens=True)
-            captions.append(caption[len(self.prompt):])
-        return captions
 
 from PIL import Image
 from transformers import AutoProcessor
