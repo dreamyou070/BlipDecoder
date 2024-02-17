26c504bef506a97f7c68366ad74a562c172b3515
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 23:47:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,121 @@ visual_encoderckpt_layer = VisionTransformer(img_size=224, patch_size=16, embed_
                                    use_grad_checkpointing=False,
                                    ckpt_layer=0,
                                    drop_path_rate=0)
+
+
+class BertLMHeadModel(BertPreTrainedModel):
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+    _keys_to_ignore_on_load_missing = [r"position_ids", r"predictions.decoder.bias"]
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.bert = BertModel(config, add_pooling_layer=False)
+        self.cls = BertOnlyMLMHead(config)
+
+        self.init_weights()
+
+    def get_output_embeddings(self):
+        return self.cls.predictions.decoder
+
+    def set_output_embeddings(self, new_embeddings):
+        self.cls.predictions.decoder = new_embeddings
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            encoder_hidden_states=None,
+            encoder_attention_mask=None,
+            labels=None,
+            past_key_values=None,
+            use_cache=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+            return_logits=False,
+            is_decoder=True,
+            reduction='mean',
+            mode='multimodal',
+    ):
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        if labels is not None:
+            use_cache = False
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds,
+                            encoder_hidden_states=encoder_hidden_states,
+                            encoder_attention_mask=encoder_attention_mask,
+                            past_key_values=past_key_values,
+                            use_cache=use_cache,
+                            output_attentions=output_attentions,
+                            output_hidden_states=output_hidden_states,
+                            return_dict=return_dict,
+                            is_decoder=is_decoder,
+                            mode=mode, )
+
+        sequence_output = outputs[0]
+        prediction_scores = self.cls(sequence_output)
+
+        if return_logits:
+            return prediction_scores[:, :-1, :].contiguous()
+
+        lm_loss = None
+        if labels is not None:
+            # we are doing next-token prediction; shift prediction scores and input ids by one
+            shifted_prediction_scores = prediction_scores[:, :-1, :].contiguous()
+            labels = labels[:, 1:].contiguous()
+            loss_fct = CrossEntropyLoss(reduction=reduction, label_smoothing=0.1)
+            lm_loss = loss_fct(shifted_prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+            if reduction == 'none':
+                lm_loss = lm_loss.view(prediction_scores.size(0), -1).sum(1)
+
+        if not return_dict:
+            output = (prediction_scores,) + outputs[2:]
+            return ((lm_loss,) + output) if lm_loss is not None else output
+
+        return CausalLMOutputWithCrossAttentions(
+            loss=lm_loss,
+            logits=prediction_scores,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+            cross_attentions=outputs.cross_attentions,
+        )
+
+    def prepare_inputs_for_generation(self, input_ids, past=None, attention_mask=None, **model_kwargs):
+        input_shape = input_ids.shape
+        # if model is used as a decoder in encoder-decoder model, the decoder attention mask is created on the fly
+        if attention_mask is None:
+            attention_mask = input_ids.new_ones(input_shape)
+
+        # cut decoder_input_ids if past is used
+        if past is not None:
+            input_ids = input_ids[:, -1:]
+
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "past_key_values": past,
+            "encoder_hidden_states": model_kwargs.get("encoder_hidden_states", None),
+            "encoder_attention_mask": model_kwargs.get("encoder_attention_mask", None),
+            "is_decoder": True,
+        }
+
+    def _reorder_cache(self, past, beam_idx):
+        reordered_past = ()
+        for layer_past in past:
+            reordered_past += (tuple(past_state.index_select(0, beam_idx) for past_state in layer_past),)
+        return reordered_past
+
+
 class BLIP_Decoder(nn.Module):
     def __init__(self,
                  med_config='configs/med_config.json',
@@ -41,14 +156,17 @@ class BLIP_Decoder(nn.Module):
 
     def forward(self, image, caption):
 
+        # [1] img
         image_embeds = self.visual_encoder(image)
         image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(image.device)
+
+        # [2] text
         text = self.tokenizer(caption, padding='longest', truncation=True, max_length=40, return_tensors="pt").to(image.device)
         text.input_ids[:, 0] = self.tokenizer.bos_token_id
-
         decoder_targets = text.input_ids.masked_fill(text.input_ids == self.tokenizer.pad_token_id, -100)
         decoder_targets[:, :self.prompt_length] = -100
 
+        # [3] forward
         decoder_output = self.text_decoder(text.input_ids,
                                            attention_mask=text.attention_mask,
 
@@ -61,18 +179,8 @@ class BLIP_Decoder(nn.Module):
         return loss_lm
 
 
-from PIL import Image
-from transformers import AutoProcessor
 
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-
-img_dir = '/home/dreamyou070/MyData/anomal_source/dtd_images/banded/banded_0002.jpg'
-image = Image.open(img_dir)
-inputs = processor(text=["a photo of a cat", "a photo of a dog"],
-                   images=image, return_tensors="pt", padding=True)
-img = inputs['pixel_values']
-txt = inputs['input_ids']
-attention_mask = inputs['attention_mask']
-print (f'input img : {img.shape}, input txt : {txt.shape}')
+img = torch.randn(1,3,224,224)
+caption = 'cat'
 caption_moel = BLIP_Decoder()
 result = caption_moel(image=img, caption='cat')
